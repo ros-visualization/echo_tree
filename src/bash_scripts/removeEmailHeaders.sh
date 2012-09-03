59975d766538519ce3e63301f925a994844e7388
Andreas Paepcke <paepcke@cs.stanford.edu>
2012-09-03 12:39:26 -0700

    Finished creation of entire Enron collection with some cleaning of the emails.

--- src/bash_scripts/removeEmailHeaders.sh
+++ src/bash_scripts/removeEmailHeaders.sh
@@ -5,21 +5,38 @@
 #
 #    o Email headers, and lines containing email-header-type words,
 #      like 'Content-type: ' even in the middle of the lines.
+#    o Empty lines
+#    o Quoted lines
+#    o All email addresses (b/c not wanted as tokens)
 #
 # The new files are named <originalName>_NoHeads.<originalExtension>
-# Uses Unix sed for speed.
+# Uses Unix sed for speed. Files in the directory that match an 
+# optional exclusion pattern are ignored.
+# Example: pattern: '.*_NoHeads.*' ignores email2_NoHeads.txt
 
 
-if [ $# -ne 1 ] 
+if [ $# -lt 1 ] 
 then
-  echo "Usage: removeEmailHeaders <directoryOfEmailFiles>"
+  echo "Usage: removeEmailHeaders <directoryOfEmailFiles> [<exclusionRegExp>]"
   exit
 fi
 
 FILES=$1/*
+if [ $# -gt 1 ]
+then
+    excludePattern=$2
+fi
+
+#echo "Exclude pattern: '$excludePattern'"
 
 for file in $FILES
 do
+    # Is file to be excluded?
+    if [ $excludePattern ] && [[ $file =~ $excludePattern ]]
+    then 
+	#echo Match
+	continue
+    fi
     dirname=${file%/*}
     #echo "Dirname: $dirname"
     fileName=${file##*/}
@@ -34,23 +51,52 @@ do
 
     # Get rid of email headers.
     # The -r turns on extended regex, which adds the
-    # '+':
+    # '+'. Each sed portion below looks for one pattern
+    # on each email line, and deletes that line if it
+    # matches the pattern:
     sed -r '
       # Email headers (some only at start of line ('^'), 
       # some anywhere in a line:
       /Content-Transfer-Encoding: .*/ d
       /Return-path: .*/ d
-      /^From: .*/ d
+      /From: .*/ d
       /Full-name: .*/ d
       /Message-ID: .*/ d
       /^Date: .*/ d
-      /^To: .*/ d
+      /^Sent: .*/ d
+      /To: .*/ d
+      /Cc: .*/ d
+      /Bcc: .*/ d
       /MIME-Version: .*/ d
       /Content-Type: .*/ d
-      /X-Mailer: .*/ d
+      /X-Mailer:.*/ d
+      /X-From: .*/ d
+      /X-To:.*/ d
+      /X-cc:.*/ d
+      /X-bcc:.*/ d
+      /X-Folder:.*/ d
+      /X-Origin:.*/ d
+      /X-FileName:.*/ d
+      /Mime-Version: .*/ d
+
+      /.*-----Original Message-----.*/ d
 
       # Email addresses:
       #   /[\da-zA-Z]+@[\da-zA-Z]+/ d
+
+      # Empty lines:
+      /^$/ d      
+
+      # Quoted lines:
+      /^>/ d
+
+      # Email addresses (leaves empty line if only
+      # email addresses, commas, and whitespace are
+      # present. Not sure how to fix:
+      s/[\da-zA-Z._]+@[\da-zA-Z._]+[\s,]*//g
+
       ' <$file >$outputName
+
+      
 done
 echo Done!
--- src/echo_tree/make_database.py
+++ src/echo_tree/make_database.py
@@ -3,9 +3,8 @@
 import os;
 import sys;
 import string;
+import argparse;
 from collections import OrderedDict;
-
-from optparse import OptionParser;
 
 # TODO: write tokenize()
 #       write dumpToDB()
@@ -23,9 +22,19 @@ class DBCreator(object):
     DELETE_EMBEDDED_HEADERS = True
     
     def __init__(self, mailDirRoot, targetDBPath):
+        '''
+        Prepare for creating the database.
+        @param mailDirRoot: fully qualified directory of raw-email root directory 
+        @type mailDirRoot: string
+        @param targetDBPath: fully qualified filename of final SQLite database
+        @type targetDBPath: string
+        '''
         self.mailDirRoot = mailDirRoot
         self.targetDBPath = targetDBPath
-        self.emailFeeder = EmailMessageFeeder(mailDirRoot, fileContentCleaner=DBCreator.cleanEmailMessageFile);
+        if os.path.isdir(targetDBPath):
+            raise ValueError("Target db path must be a file name, not, for example, a directory");
+        self.targetDBDir  = os.path.dirname(os.path.realpath(targetDBPath));
+        #self.emailFeeder = EmailMessageFeeder(mailDirRoot, fileContentCleaner=DBCreator.cleanEmailMessageFile);
         self.currentSentenceID = 0;
 
     def createInMemoryWordStats(self, numMessages=None):
@@ -36,17 +45,29 @@ class DBCreator(object):
             for token in tokenArray:
                 WordIndex.addPosting(WordPosting(token.word, token.sentenceID));
 
-    def createCleanEmailCollection(self, outputDirPath, filePrefix='email', maxContent=None):
+
+    def createEmailCollection(self, outputDirPath, filePrefix='email', maxContent=None, deleteEmailHeaders=True, deleteFilesWithHeaders=True):
         '''
-        Pulls all email messages through the EmailMessageFeeder, clearing 
-        out the headers. Creates a series of EMAIL_FILE_CHUNCK_SIZE files
-        with the resulting cleaned email messages. Within files messages are
-        separated with a standard string that contains a serial number
-        of the following email message. The string is constructed by
+        Pulls all email messages from a directory tree, using the EmailMessageFeeder class.
+        Creates a series of EMAIL_FILE_CHUNCK_SIZE files with the resulting email messages. 
+        Within these chunk files messages are separated with a standard string that contains 
+        a serial number of the following email message. The string is constructed by
         concatenating EMAL_SEPARATOR_PREFIX<serialNumber>EMAIL_SEPARATOR_POSTFIX.
         Example:
         
         #*/123*/-------------------------#^@\n
+        
+        The chunks are named <filePrefix><serialNumber>.txt
+        
+        If deleteEmailHeaders is True, each chunk of email messages is scanned by
+        Unix stream editor sed to remove email headers. A new chunk file is created
+        and named <filePrefix><serialNumber>_NoHeads.txt.
+        
+        If deleteFilesWithHeaders is True, the initial, headers-containing chunk files
+        are deleted after the headers are cleaned out. Else the files are retained.
+        Note that this roughly doubles the size of the directory. 
+        
+        
         
         @param outputDirPath: path to where output files will be stored
         @type outputDirPath: string
@@ -54,6 +75,14 @@ class DBCreator(object):
         @type filePrefix: string
         @param maxContent: limit to number of bytes worth of email messages to process.
         @type maxContent: int
+        @param deleteEmailHeaders: if True, email headers will be deleted from the results
+        @type deleteEmailHeaders: boolean
+        @param deleteFilesWithHeaders: if True, and if headerless versions were created
+                                       (i.e. deleteEmailHeaders was True), then the initial
+                                       email files that include headers will be deleted.
+        @type deleteFilesWithHeaders: boolean
+        @return: number of email messages collected, and total number of bytes
+        @rtype: (int,int)
         @raises: ValueError if output directory does not exist
         '''
         
@@ -61,37 +90,79 @@ class DBCreator(object):
         if not os.path.isdir(outputDirPath):
             raise IOError("Path '%s' is not an existing directory." % outputDirPath);
         
-        feeder = EmailMessageFeeder(self.mailDirRoot, fileContentCleaner=DBCreator.cleanEmailMessageFile);
+        # Print a progress message every REPORT_INTERVAL processed bytes:
+        REPORT_INTERVAL = 50000;
+        
+        feeder = EmailMessageFeeder(self.mailDirRoot);
+        if maxContent is not None:
+            maxContent = int(maxContent);
         emailIndex = -1;
         currFileIndex = -1;
         currFileFD = None;
+        latestProgressReportAt = 0;
         # Force new file as first action in loop below:
         contentLenThisFile = EMAIL_FILE_CHUNK_SIZE + 1;
+        createdFilesList = [];
         # Keep track of overall bytes processed (across all output files):
         contentLenTotal = 0;
-        for email in feeder:
-            if contentLenThisFile >= EMAIL_FILE_CHUNK_SIZE:
-                if currFileFD is not None:
-                    currFileFD.close();
-                currFileIndex += 1;
-                currFilePath = os.path.join(outputDirPath, filePrefix + str(currFileIndex) + '.txt');
-                try:
-                    currFileFD = open(currFilePath, 'w');
-                except IOError as e:
-                    raise IOError("Cannot open new email file '%s' for writing: %s" % (currFilePath, `e`));
-                contentLenThisFile = 0;     
-                
-            # Email separator:
-            emailIndex += 1;
-            sep = EMAIL_SEPARATOR_PREFIX + str(emailIndex) + EMAIL_SEPARATOR_POSTFIX;
-            currFileFD.write(sep);
-            currFileFD.write(email);
-            currFileFD.write('\n');
-            contentLenThisFile += len(email);
-            contentLenTotal += len(email);
-            if maxContent is not None and contentLenTotal >= maxContent:
-                return;
+        try:
+            for email in feeder:
+                # Has this chunk file reached its max size:
+                if contentLenThisFile >= EMAIL_FILE_CHUNK_SIZE:
+                    # Close the just finished file:
+                    if currFileFD is not None:
+                        currFileFD.close();
+                    currFileIndex += 1;
+                    # Start a new chunk file:
+                    currFilePath = os.path.join(outputDirPath, filePrefix + str(currFileIndex) + '.txt');
+                    try:
+                        currFileFD = open(currFilePath, 'w');
+                        createdFilesList.append(currFilePath);
+                    except IOError as e:
+                        raise IOError("Cannot open new email file '%s' for writing: %s" % (currFilePath, `e`));
+                    contentLenThisFile = 0;     
+                    
+                # Email separator:
+                emailIndex += 1;
+                sep = EMAIL_SEPARATOR_PREFIX + str(emailIndex) + EMAIL_SEPARATOR_POSTFIX;
+                currFileFD.write(sep);
+                currFileFD.write(email);
+                currFileFD.write('\n');
+                contentLenThisFile += len(email);
+                contentLenTotal += len(email);
+                if (contentLenTotal - latestProgressReportAt) > REPORT_INTERVAL:
+                    print "Processed %.3f KB" % (contentLenTotal / 1000);
+                    sys.stdout.flush();
+                    latestProgressReportAt = contentLenTotal;
+                if maxContent is not None and contentLenTotal >= maxContent:
+                    return (emailIndex + 1, contentLenTotal);
+        finally:
+            currFileFD.close();
+            # Are we to delete email headers?
+            if deleteEmailHeaders:
+                self.deleteEmailHeaders(outputDirPath);
+                # Are we to delete the initial chunk files?
+                if deleteFilesWithHeaders:
+                    for initialChunkFile in createdFilesList:
+                        try:
+                            os.remove(initialChunkFile);
+                        except IOError:
+                            print "Could not delete initial chunk file '%s'" % initialChunkFile;
+        return (emailIndex + 1, contentLenTotal);
              
+    def deleteEmailHeaders(self, emailDirectory):
+        '''
+        Go through each file in the given directory, ignoring files with "_NoHeads" in the name.
+        Run each file through the Unix sed editor to remove email headers. That script creates
+        files with email headers removed. Those files are named the original names, with '_NoHeads'
+        added.
+        @param emailDirectory: directory containing files with any number of email messages.
+        @type emailDirectory: string
+        '''
+        os.system(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../bash_scripts/removeEmailHeaders.sh %s %s' % (emailDirectory, '.*_NoHead.*')));        
+        
+        
+        
     def tokenize(self, msg):
         '''
         Given an email message as a string, partition the msg into sentences,
@@ -106,6 +177,7 @@ class DBCreator(object):
     @staticmethod
     def cleanEmailMessageFile(contentStr):
         '''
+        Obsolete: done with Unix sed stream editor now.
         Given one email message, remove its header, except for the content
         of the Subject line. If DBCreator.DELETE_EMBEDDED_HEADERS is True,
         goes through entire message and deletes lines starting with any of
@@ -272,9 +344,13 @@ class WordIndex(object):
 
 class WordPosting(object):
     '''
-    A single posting. It contains the word itself, a set of sentence IDs in
-    which the word occurred, an occurrence count, and a dictionary of follow-on
-    words. May be used as an iterator: for followPosting in oneWordPosting:...
+    A single posting. It contains:
+        - the word itself, 
+        - a set of sentence IDs in which the word occurred,
+        - a set of message IDs in which the word occurred, 
+        - a total occurrences count, and 
+        - a dictionary of follow-on words. 
+    May be used as an iterator: for followPosting in oneWordPosting:...
     '''
     
     def __init__(self, word, sentenceID):
@@ -321,14 +397,17 @@ class WordPosting(object):
 class EmailMessageFeeder(object):
     '''
     Iterator that feeds one email message after the other to a caller.
-    Input is the root directory of a file system tree at that contains
-    files that each contain one email message. The optionally passed-in
+    Input is the root directory of a file system tree containing
+    files that each hold one email message. The optionally passed-in
     function fileContentCleaner is called with each email. 
     '''
 
     def __init__(self, mailDirRoot, fileContentCleaner=None):
         '''
-        Create email message iterator.
+        Create email message iterator. Every call to 'next()' will deliver a string
+        that is one email message. Assumption is that files in mailDirRoot
+        are email messages, one per file. 
+
         @param mailDirRoot: path to root directory of email files. 
         @type mailDirRoot: string
         @param fileContentCleaner: optional function to call with each email message as a string.
@@ -339,7 +418,7 @@ class EmailMessageFeeder(object):
         self.currFileIndex = 0;
         self.filePaths = self.allFiles();
         self.numMsgs = len(self.filePaths);
-
+        
     def getNumMsgs(self):
         return self.numMsgs;
 
@@ -375,28 +454,52 @@ class EmailMessageFeeder(object):
             self.currFileIndex += 1;
         return fileContents;
     
-# --------------------------  Tests -----------------------
+# --------------------------  Main -----------------------
 
 if __name__ == '__main__':
     
-    parser = OptionParser();
-    #parser.add_option("-d", "--dirIn", dest="emailsDirName");
-    #parser.add_option("-o", "--fileOut", dest="dbOutFile");
-    parser.add_option("-t", "--test", dest="testing");
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("mailDirRoot", help="Root of dir tree with emails at leaves.");
+    parser.add_argument("dbOutFile", help="Full path to final database file");
     
-    (options, args) = parser.parse_args()
-    if (len(args) != 2) and testing is None:
-        print "Usage: make_database { -t/--test | <emailDirPath> <mySqlOutFilePath>}";
+    parser.add_argument("-m", "--maxBytes", type=int,
+                        help="max number of bytes to process.");
+    parser.add_argument("-c", "--clearHeaders", action="store_true",
+                        help="flag to delete email headers.");
+    parser.add_argument("-d", "--delRawFiles", action="store_true",
+                        help="flag to delete raw email files if versions with no headers were created.");
+    
+    
+    args = parser.parse_args()    
+#    print "Mail dir root: " + args.mailDirRoot;
+#    print "dbOutFile: " + args.dbOutFile;
+#    print "max num: " + str(args.maxBytes);
+#    print "clear Header: " + str(args.clearHeaders);
+#    print "delRawFiles: " + str(args.delRawFiles);
+
+    if os.path.isdir(args.dbOutFile):
+        print "Second argument must be a target SQLite database file name, not, for example, a directory.";
         sys.exit();
+
+    dbCreator = DBCreator(args.mailDirRoot, args.dbOutFile);
+    collectedEmailsDir = os.path.dirname(os.path.realpath(args.dbOutFile));
+    
         
-    #print "Args: '%s'" % args
-    #print "Options: '%s'" % options
-    #sys.exit()
+    (numEmails, numBytes) = dbCreator.createEmailCollection(dbCreator.targetDBDir, 
+                                                            filePrefix='email', 
+                                                            maxContent=args.maxBytes, 
+                                                            deleteEmailHeaders=args.clearHeaders, 
+                                                            deleteFilesWithHeaders=args.delRawFiles);
     
-    emailsDir = args[0];
-    dbOutFile = args[1];
-    dbCreator = dbCreator(emailsDir, dbOutFile);
+    if numBytes < 1000000:
+        print 'Processed %d emails (%.3f KB)' % (numEmails, numBytes / 1000);
+    else:
+        print 'Processed %d emails (%.3f MB)' % (numEmails, numBytes / 1000000);
+    sys.exit();
     
+    # ------------------------------------------------------------------------------------------
+    # The -t or --test option was given to the script implementation. Do unittesting:
     
     import unittest
     import shutil
